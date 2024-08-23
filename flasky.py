1c5ecd3cfd5e91b8a11fa76849a635abf38a792e
RamonMarM <120236024+RamonMarM@users.noreply.github.com>
2024-08-22 21:01:56 -0300

    Update flasky.py

--- flasky.py
+++ flasky.py
@@ -3,7 +3,6 @@ import click
 from flask_migrate import Migrate
 from app import create_app, db
 from app.models import User, Role
-import requests
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
